24a9e56b972681f2c5268049d6e797167e7e56a0
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-09-20 22:40:46 +0100

    SharpVids: Add a repository for handling video models

---
+++ SharpVids/Data/Repositories/IVideoRepository.cs
@@ -0,0 +1,11 @@
+﻿using SharpVids.Models;
+
+namespace SharpVids.Data.Repositories;
+
+public interface IVideoRepository
+{
+    public Task<List<VideoModel>> GetVideosAsync();
+    public Task<VideoModel?> GetVideoByIdAsync(Guid id);
+
+    public Task AddVideoAsync(VideoModel video);
+}
---
+++ SharpVids/Data/Repositories/VideoRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpVids.Models;
+
+namespace SharpVids.Data.Repositories;
+
+public sealed class VideoRepository : IVideoRepository
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public VideoRepository(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public async Task<List<VideoModel>> GetVideosAsync()
+    {
+        return await _databaseContext.Videos.ToListAsync();
+    }
+
+    public async Task<VideoModel?> GetVideoByIdAsync(Guid id)
+    {
+        var video = await _databaseContext.Videos.FirstOrDefaultAsync(v => v.Id == id);
+        return video;
+    }
+
+    public async Task AddVideoAsync(VideoModel video)
+    {
+        await _databaseContext.Videos.AddAsync(video);
+        await _databaseContext.SaveChangesAsync();
+    }
+}
