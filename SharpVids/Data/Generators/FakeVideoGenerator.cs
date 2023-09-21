943bb01fbec5b39fbf84c545f520656e3ebabf3c
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-09-21 20:09:35 +0100

    SharpVids: Add a simple fake video model generator
    Currently it doesn't generate any video bytes or upholds any
    relationships (CreatorId).

---
+++ SharpVids/Data/Generators/FakeVideoGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using SharpVids.Models;
+
+namespace SharpVids.Data.Generators;
+
+public sealed class FakeVideoGenerator
+{
+	public FakeVideoGenerator()
+	{
+		_faker.RuleFor(u => u.Id, f => Guid.NewGuid())
+			  // TODO: Add constructor argument for a user if we need it
+			  .RuleFor(u => u.CreatorId, f => Guid.NewGuid())
+			  .RuleFor(u => u.Title, f => f.Hacker.Phrase())
+			  .RuleFor(u => u.Description, f => f.Hacker.Phrase())
+			  .RuleFor(u => u.UploadDate, f => f.Date.PastOffset().UtcDateTime)
+			  // TODO: Proper aspect ratio for when we display thumbnails
+			  .RuleFor(u => u.ThumbnailUrl, f => f.Image.PicsumUrl(80, 80));
+			  // TODO: Maybe generate a fake video/premade video
+	}
+
+	public IEnumerable<VideoModel> GenerateForever()
+	{
+		return _faker.GenerateForever();
+	}
+
+	private readonly Faker<VideoModel> _faker = new();
+}
