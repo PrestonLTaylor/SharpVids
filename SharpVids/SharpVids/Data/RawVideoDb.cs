a4906c01c2ec733abf6ed50dcccaaa1d4de1a60a
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-06-17 16:02:19 +0100

    SHAR-7 Implement a class for connecting to our MongoDB service
    The RawVideoDb class is where we hold the connection to our MongoDB
    service.
    
    It is injectable and has a transient lifetime to be used by other
    services to interface without MongoDB service.

---
+++ SharpVids/SharpVids/Data/RawVideoDb.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Driver;
+
+namespace SharpVids.Data;
+
+/// <summary>
+/// A wrapper for connecting to our MongoDB service to be used as a raw video database.
+/// </summary>
+public sealed class RawVideoDb
+{
+    public RawVideoDb(ILogger<RawVideoDb> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        Client = TryToConnectToRawVideoDb(configuration);
+    }
+
+    private MongoClient TryToConnectToRawVideoDb(IConfiguration configuration)
+    {
+        var connectionString = TryToGetRawVideoDbConnectionString(configuration);
+
+        try
+        {
+            return new MongoClient(connectionString);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex, "Failed to connect to raw video db.");
+            throw;
+        }
+    }
+
+    private string TryToGetRawVideoDbConnectionString(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString(RAW_VIDEO_DB_CONNECTION_STRING_NAME);
+        if (connectionString is null)
+        {
+            var ex = new InvalidOperationException($"Unable to find connection string '{RAW_VIDEO_DB_CONNECTION_STRING_NAME}' for raw video db.");
+            _logger.LogCritical(ex, "Failed to connect to raw video db.");
+            throw ex;
+        }
+
+        return connectionString;
+    }
+
+    public MongoClient Client { get; }
+
+    const string RAW_VIDEO_DB_CONNECTION_STRING_NAME = "raw-video-db";
+
+    private readonly ILogger<RawVideoDb> _logger;
+}
+
+public static class RawVideoDbInstaller
+{
+    public static IServiceCollection AddRawVideoDb(this IServiceCollection services)
+    {
+        services.AddTransient<RawVideoDb>();
+
+        return services;
+    }
+}
