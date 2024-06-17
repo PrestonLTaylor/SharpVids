bace78d596a43482cf28cee198c1d5e7ca0c9498
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-06-17 16:02:19 +0100

    SHAR-7 Implement a class for connecting to our MongoDB service
    The RawVideoDb class is where we hold the connection to our MongoDB
    service.
    
    It is injectable and has a transient lifetime to be used by other
    services to interface without MongoDB service.

--- SharpVids/SharpVids/Program.cs
+++ SharpVids/SharpVids/Program.cs
@@ -1,4 +1,5 @@
 using SharpVids.Components;
+using SharpVids.Data;
 using SharpVids.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRawVideoDb();
 
 builder.Services.Configure<UploadOptions>(builder.Configuration.GetSection(UploadOptions.Section));
 
