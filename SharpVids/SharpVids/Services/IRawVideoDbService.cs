22d3a6aff87bb058a98005d5b48f43e117cc15a9
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-06-18 16:38:53 +0100

    SHAR-7 Create a metadata document when uploading a video
    We now have a collection for raw video metadatas. This allows us to link
    any metadata we need to a raw video to be used after being encoded.

--- SharpVids/SharpVids/Services/IRawVideoDbService.cs
+++ SharpVids/SharpVids/Services/IRawVideoDbService.cs
@@ -5,5 +5,17 @@ namespace SharpVids.Services;
 
 public interface IRawVideoDbService
 {
+    /// <summary>
+    /// Uploads a raw video to our raw video database.
+    /// </summary>
+    /// <param name="videoFile">The <see cref="IBrowserFile"/> a user is trying to upload.</param>
+    /// <param name="updateCallback">A callback that is called when the number of bytes uploaded has changed.</param>
+    /// <returns>An <see cref="ObjectId"/> used to reference the video file inside of our database.</returns>
     public Task<ObjectId> UploadRawVideoAsync(IBrowserFile videoFile, Action<long> uploadCallback);
+
+    /// <summary>
+    /// Creates a video metadata document inside of our video metadata collection.
+    /// </summary>
+    /// <param name="videoId">The id of the raw video inside of GridFS.</param>
+    public Task AddVideoMetadataAsync(ObjectId videoId);
 }
